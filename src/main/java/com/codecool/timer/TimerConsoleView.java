f6c80c82a949cec963fe37e2a88ce246b8a1d0d9
lpelczar <lukepelczar@gmail.com>
2018-06-07 15:10:10 +0200

    Implement getting command method

--- src/main/java/com/codecool/timer/TimerConsoleView.java
+++ src/main/java/com/codecool/timer/TimerConsoleView.java
@@ -1,9 +1,12 @@
 package com.codecool.timer;
 
+import java.util.Scanner;
+
 public class TimerConsoleView implements TimerView {
 
     @Override
     public String getCommand() {
-        return null;
+        System.out.println("Enter command.");
+        return new Scanner(System.in).nextLine();
     }
 }
